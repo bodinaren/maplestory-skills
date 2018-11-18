cbc6d53088e1fe04a70ab88fb57b154201fc448c
Marcus Bodin <bodinaren@gmail.com>
2018-11-18 15:23:36 +0100

    (archer): add archer values

--- src/global/values/archer.ts
+++ src/global/values/archer.ts
@@ -1,64 +1,64 @@
 export const AgileArcherValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  piercing: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  evasion: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [31, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49],
+  piercing: [1, 1, 1.6, 2.2, 2.8, 3.4, 4.0, 4.6, 5.2, 5.8, 6.4],
+  evasion: [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 };
 
 export const ArrowBarrageValues = {
   minLevel: 1,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  firstDamage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  thirdDamage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [0, 0, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28],
+  firstDamage: [153, 153, 164, 175, 186, 197, 208, 219, 230, 241, 252],
+  thirdDamage: [82, 82, 87, 92, 97, 102, 107, 112, 117, 122, 127],
 };
 
 export const ArrowStormValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  damage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [25, 27, 29, 31, 33, 35, 37, 39, 41, 43],
+  damage: [39, 42, 45, 48, 51, 54, 57, 60, 63, 66],
 };
 
 export const ArrowStreamValues = {
   minLevel: 1,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  damage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  hits: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [0, 0, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28],
+  damage: [32, 34, 36, 38, 40, 42, 44, 46, 48, 50],
+  hits: [3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5],
 };
 
 export const BowSwingValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  damage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  additionalDamage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [40, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58],
+  damage: [244, 244, 261, 278, 295, 312, 329, 346, 363, 380, 397],
+  additionalDamage: [183, 183, 196, 209, 222, 235, 248, 261, 274, 287, 300],
 };
 
 export const BronzeEagleValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  dexterity: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [13, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31],
+  dexterity: [4.4, 4.4, 4.7, 5, 5.3, 5.6, 5.9, 6.2, 6.5, 6.8, 7.1],
 };
 
 export const ConditioningValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  attack: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  movement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [19, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37],
+  attack: [1.0, 1.0, 1.6, 2.2, 2.8, 3.4, 4.0, 4.6, 5.2, 5.8, 6.4],
+  movement: [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 };
 
 export const EagleClawValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  damage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  additionalDamage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  movement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [28, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46],
+  damage: [302, 302, 323, 344, 365, 386, 407, 428, 449, 470, 491],
+  additionalDamage: [60, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96],
+  movement: [30, 30, 30, 30, 35, 35, 35, 40, 40, 40, 45],
 };
 
 export const EagleGlideValues = {
@@ -69,62 +69,56 @@ export const EagleGlideValues = {
 export const EaglesMajestyValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  spirit: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  damage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [37, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55],
+  spirit: [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  damage: [25, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34],
 };
 
 export const EvasiveSalvoValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  damage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [10, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28],
+  damage: [108, 108, 115, 122, 129, 136, 143, 150, 157, 164, 171],
 };
 
 export const IceArrowValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  damage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-};
-
-export const PoisonVialValues = {
-  minLevel: 0,
-  maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [22, 24, 26, 28, 30, 32, 34, 36, 38, 40],
+  damage: [58, 62, 66, 70, 74, 78, 82, 86, 90, 94],
 };
 
 export const PrecisionShooterValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  speed: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  screwdriver: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [46, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64],
+  speed: [1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
+  screwdriver: [5, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
 };
 
 export const RapidShotValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  damage: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [16, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34],
+  damage: [42, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69],
 };
 
 export const ScrewdriverShotValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  stage1: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  stage2: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  stage3: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [43, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61],
+  stage1: [372, 372, 399, 426, 453, 480, 507, 534, 561, 588, 615],
+  stage2: [725, 725, 777, 829, 881, 933, 985, 1037, 1089, 1141, 1193],
+  stage3: [1078, 1078, 1155, 1232, 1309, 1386, 1463, 1540, 1617, 1694, 1771],
 };
 
 export const SharpEyesValues = {
   minLevel: 0,
   maxLevel: 10,
-  levelRequirement: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  criticalAllies: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  accuracy: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  criticalSelf: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  levelRequirement: [34, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52],
+  criticalAllies: [13, 13, 20, 27, 34, 41, 48, 55, 62, 69, 76],
+  accuracy: [1, 1, 1, 2, 2, 3, 3, 4, 4, 4, 5],
+  criticalSelf: [4, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40],
 };
 
 export const SnipeValues = {
