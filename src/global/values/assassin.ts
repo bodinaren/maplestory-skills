4a6185e88c0d7f1a0f8edbf4cd2ebdc8c2fdd0ab
Marcus Bodin <bodinaren@gmail.com>
2018-11-19 06:10:31 +0100

    (assassin): add values

---
+++ src/global/values/assassin.ts
@@ -0,0 +1,119 @@
+export const DarkCloakValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [13, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31],
+  movement: [21, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39],
+  cooldown: [33, 33, 31, 29, 27, 25, 23, 21, 19, 17, 15],
+};
+
+export const DashValues = {
+  minLevel: 1,
+  maxLevel: 1,
+};
+
+export const DeathSentenceValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [34, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52],
+  damage: [83, 83, 88, 93, 98, 103, 108, 113, 118, 123, 128],
+};
+
+export const FatalStrikesValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [16, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34],
+  duration: [8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+};
+
+export const FragmentedStarsValues = {
+  minLevel: 1,
+  maxLevel: 10,
+  levelRequirement: [0, 0, 10, 12, 14, 16, 18, 20, 22, 24, 26],
+  damage: [215, 215, 229, 243, 257, 271, 285, 299, 313, 327, 341],
+};
+
+export const LuckyStarsValues = {
+  minLevel: 1,
+  maxLevel: 10,
+  levelRequirement: [0, 0, 10, 12, 14, 16, 18, 20, 22, 24, 26],
+  damage: [76, 76, 81, 86, 91, 96, 101, 106, 111, 116, 121],
+  aoe: [76, 76, 81, 86, 91, 96, 101, 106, 111, 116, 121],
+};
+
+export const MarkOfDeathValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [25, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43],
+  damage: [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+};
+
+export const MirrorImageDarkBladeValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [46, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64],
+  damage: [236, 236, 252, 268, 284, 300, 316, 332, 348, 364, 380],
+};
+
+export const ShadowArtsValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [43, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61],
+  damage: [2, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
+};
+
+export const ShadowBurstValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [22, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40],
+  damage: [211, 211, 225, 239, 253, 267, 281, 295, 309, 323, 337],
+  additionalDamage: [211, 211, 225, 239, 253, 267, 281, 295, 309, 323, 337],
+};
+
+export const ShadowChaserValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [19, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37],
+  damage: [39, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66],
+};
+
+export const ShadowCutterValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [10, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28],
+  damage: [99, 99, 106, 113, 120, 127, 134, 141, 148, 155, 162],
+};
+
+export const ShadowWebValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [37, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55],
+  damage: [28, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46],
+  additionalDamage: [14, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+  movement: [10, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28],
+};
+
+export const SoulGrindValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [31, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49],
+  damage: [97, 97, 103, 109, 115, 121, 127, 133, 139, 145, 151],
+};
+
+export const StarChaserValues = {
+  minLevel: 1,
+  maxLevel: 1,
+};
+
+export const StarFlurryValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [28, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46],
+  damage: [59, 59, 62, 65, 68, 71, 74, 77, 80, 83, 86],
+};
+
+export const ThrownWeaponMasteryValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [40, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58],
+  attack: [1, 1, 1.4, 1.8, 2.2, 2.6, 3.0, 3.4, 3.8, 5.8, 6.4],
+};
