eb488ef75a1bf1f4ae64883f6f51825ae1d1b104
Marcus Bodin <bodinaren@gmail.com>
2018-11-22 08:30:06 +0100

    (knight): add values

---
+++ src/global/values/knight.ts
@@ -0,0 +1,122 @@
+export const BulwarkValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [34, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52],
+  allies: [4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+  cooldown: [130, 130, 120, 110, 100, 90, 80, 70, 60, 55, 50],
+};
+
+export const CrossCutValues = {
+  minLevel: 1,
+  maxLevel: 10,
+  levelRequirement: [0, 0, 10, 12, 14, 16, 18, 20, 22, 24, 26],
+  damage: [79, 79, 85, 91, 97, 103, 109, 115, 121, 127, 133],
+};
+
+export const DefenderOfTheFaithValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [40, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58],
+  allies: [4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+  cooldown: [130, 130, 120, 110, 100, 90, 80, 70, 60, 55, 50],
+};
+
+export const DivineStrikeValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [13, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31],
+  damage: [501, 501, 536, 571, 606, 641, 676, 711, 746, 781, 816],
+};
+
+export const DrillThrustValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [37, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55],
+  damage: [412, 412, 441, 470, 499, 528, 557, 586, 615, 644, 673],
+};
+
+export const IronDefenseValues = {
+  minLevel: 1,
+  maxLevel: 1,
+};
+
+export const IronShieldValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [10, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28],
+  reduction: [25, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70],
+};
+
+export const LongswordMasteryValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [43, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61],
+  increase: [1.0, 1.0, 1.6, 2.2, 2.8, 3.4, 4.0, 4.6, 5.2, 5.8, 6.4],
+};
+
+export const ShieldBoosterValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [46, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64],
+  damage: [190, 190, 204, 218, 232, 246, 260, 274, 288, 302, 316],
+  increase: [3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+};
+
+export const ShieldChargeValues = {
+  minLevel: 1,
+  maxLevel: 1,
+};
+
+export const ShieldMasteryValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [31, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49],
+  evasion: [9, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36],
+  perfectGuard: [5, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+  increaseChance: [40, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76],
+  damage: [3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+};
+
+export const ShieldTossValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [25, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43],
+  damage: [60, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96],
+  reduction: [1.0, 1.0, 1.2, 2.2, 2.2, 3.2, 4.0, 4.2, 5.2, 5.8, 6.4],
+};
+
+export const ShieldWallValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [16, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34],
+  resistance: [90, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360],
+};
+
+export const StingingFlurryValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [28, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46],
+  damage: [83, 83, 89, 95, 101, 107, 113, 119, 125, 131, 137],
+};
+
+export const TornadoSlashValues = {
+  minLevel: 1,
+  maxLevel: 10,
+  levelRequirement: [0, 0, 10, 12, 14, 16, 18, 20, 22, 24, 26],
+  damage: [81, 81, 86, 91, 96, 101, 106, 111, 116, 121, 126],
+};
+
+export const TyphoonSlashValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [19, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37],
+  damage: [147, 147, 158, 169, 180, 191, 202, 213, 224, 235, 246],
+};
+
+export const WarhornValues = {
+  minLevel: 0,
+  maxLevel: 10,
+  levelRequirement: [22, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40],
+  allies: [4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+  increase: [3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+};
