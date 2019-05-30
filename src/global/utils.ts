000357777df854b6f569db7d8555a8ec1fca13a2
Marcus Bodin <bodinaren@gmail.com>
2019-05-30 12:09:20 +0200

    (tests): check if new Image is available

--- src/global/utils.ts
+++ src/global/utils.ts
@@ -1,19 +1,15 @@
 import { getAssetPath } from "@stencil/core";
 
+declare const window: any;
 let hasWebp;
 
 _hasWebp().then((has) => hasWebp = has);
 
 function _hasWebp(): Promise<boolean> {
   return new Promise((resolve) => {
+    if (!window.Image) return resolve(false); // for testing where `new Image()` may not be available
     const img = new Image();
-    img.onload = () => {
-      if (img.width === 2 && img.height === 1) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    };
+    img.onload = () => resolve(img.width === 2 && img.height === 1);
     img.onerror = () => resolve(false);
     img.src = "data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==";
   });
